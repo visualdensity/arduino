04a8bd80f45e752f6c57b7ab23aa4ea99f95fbe7
Wee Keat Chin <weekeat@visualdensity.com>
2012-11-21 22:33:43 +1100

    Added multiple servos just for fun

--- getting_started/servo_translator/servo_translator.ino
+++ getting_started/servo_translator/servo_translator.ino
@@ -1,16 +1,19 @@
  #include <Servo.h>
  
  Servo myservo;
+ Servo myservo2;
  
  const int POT = 4;
  
- int pos   = 0;
+ int pos1  = 0;
+ int pos2  = 0;
  int input = 0;
  
  void setup()
  {
    Serial.begin(9600);
    myservo.attach(9);
+   myservo2.attach(12);
  }
  
  void loop()
@@ -18,6 +21,10 @@
    input = analogRead(POT);
    Serial.println(input);
    
-   pos = map(input, 1,350,1,180);
-   myservo.write(pos);
+   pos1 = map(input, 1,350,1,180);
+   pos2 = map(input, 1,350,1,180);
+
+   myservo.write(pos1);
+   myservo2.write(pos2);
+
  }
