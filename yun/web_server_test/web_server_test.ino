61d39e8a671f56a52888bca15b277f4bb71ca145
Wee Keat Chin <weekeat@visualdensity.com>
2013-12-02 00:13:33 +1100

    More play with webserver <> control and feedback in  Yun

--- yun/web_server_test/web_server_test.ino
+++ yun/web_server_test/web_server_test.ino
@@ -5,16 +5,26 @@
 YunServer server;
 String startString;
 
+int led = 11;
+int ledState = 0;
+
 void setup() {
     Serial.begin(9600);
 
-    // Bridge startup
     pinMode(13,OUTPUT);
     digitalWrite(13, LOW);
 
     Bridge.begin();
     digitalWrite(13, HIGH);
 
+    Bridge.begin();
+    Console.begin();
+    
+    //while (!Console){
+    //    ; // wait for Console port to connect.
+    //}
+    //Console.println("You're connected to the Console!!!!");
+  
     server.listenOnLocalhost();
     server.begin();
 }//setup
@@ -25,14 +35,31 @@ void loop() {
     YunClient client = server.accept();
 
     if (client) {
+
         String command = client.readString();
         command.trim();
-        Serial.println(command);
+        //Console.println(command);
 
         if (command == "status") {
             client.print( analogRead(A0) );
         }
+        
+        if (command == "led/status") {
+            client.print( ledState );
+        }
+        
+        if ( command == "led/on" ) {
+            analogWrite( led, 50 );
+            ledState = 1;
+            client.print( "1" );
+        }
 
+        if ( command == "led/off" ) {
+            analogWrite( led, 0 );
+            ledState = 0;
+            client.print( "0" );
+        }
+        
         client.stop();
     }
 
